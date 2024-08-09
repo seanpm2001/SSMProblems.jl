440252c88105f5d41ed3fe51230af28f56436b31
Charles Knipp <charles.a.knipp@frb.gov>
2024-08-09 14:57:16 -0400

    added basic particle methods and filters

---
+++ examples/particle-mcmc/script.jl
@@ -0,0 +1,76 @@
+using AdvancedMH
+using CairoMakie
+
+include("simple-filters.jl")
+
+true_params, simulation_model = let T = Float32
+    θ = randexp(T, 3)
+    dyn = LinearGaussianLatentDynamics(T[1 1;0 1], diagm(θ[1:2]))
+    obs = LinearGaussianObservationProcess(T[0.5 0.5], diagm(θ[3:end]))
+    θ, StateSpaceModel(dyn, obs)
+end
+
+# simulate data
+rng = MersenneTwister(1234)
+_, data = sample(rng, simulation_model, 150)
+
+#=
+    Not crazy about this structure, especially since the RNG is referenced on
+    the global scope. I think we can make a PMCMC sampler type which includes
+    the filter algorithm within the sampler definition.
+
+    Another issue is that we lose information on the states. Granted, this is
+    also by design since that would cost a considerable amount of memory, but
+    is useful nonetheless. This also needs to interface with bundle_samples()
+    different than ususal, since we have the parameter space and the filtered
+    states.
+=#
+function density(θ::Vector{T}) where {T<:Real}
+    if all(θ -> θ > 0.0, θ)
+        dyn = LinearGaussianLatentDynamics(T[1 1;0 1], diagm(θ[1:2]))
+        obs = LinearGaussianObservationProcess(T[0.5 0.5], diagm(θ[3:end]))
+        _, ll = sample(rng, StateSpaceModel(dyn, obs), data, KF())
+        return ll
+    else
+        return -Inf
+    end
+end
+
+# plug it into the DensityModel interface for now
+pmmh  = RWMH(MvNormal(zeros(Float32, 3), (0.01f0)*I))
+model = DensityModel(density)
+
+# works with AdvancedMH out of the box
+chains = sample(model, pmmh, 100_000)
+burn_in = 10_000
+
+# plot the posteriors
+hist_plots = begin
+    param_post = hcat(getproperty.(chains[burn_in:end], :params)...)
+    fig = Figure(size = (1200, 400))
+
+    for i in 1:3
+        # plot the posteriors with burn-in
+        hist(
+            fig[1, i],
+            param_post[i, :],
+            color = :gray,
+            strokewidth = 1,
+            normalization = :pdf
+        )
+
+        # plot the true values
+        vlines!(
+            fig[1, i],
+            true_params[i],
+            color = :red,
+            linestyle = :dash,
+            linewidth = 2
+        )
+    end
+
+    fig
+end
+
+# this is useful for SMC algorithms like SMC² or density tempered SMC
+acc_ratio = mean(getproperty.(chains, :accepted))
---
+++ examples/particle-mcmc/simple-filters.jl
@@ -0,0 +1,282 @@
+using SSMProblems
+using GaussianDistributions
+using PDMats, LinearAlgebra
+using Distributions
+using Random
+using UnPack
+using StatsFuns
+
+import AbstractMCMC: sample, AbstractSampler
+
+## UTILITIES ##################################################################
+
+# GaussianDistributions.correct will error when type casting otherwise
+function Base.convert(::Type{PDMat{T, MT}}, mat::MT) where {MT<:AbstractMatrix, T<:Real}
+    return PDMat(Symmetric(mat))
+end
+
+# necessary for type stability of logpdf for Gaussian
+function Distributions.logpdf(P::Gaussian, x)
+    dP = length(P.μ)
+    logdetcov = GaussianDistributions._logdet(P.Σ, dP)
+    return -(GaussianDistributions.sqmahal(P,x) + logdetcov + dP*convert(eltype(x), log2π))/2
+end
+
+function multinomial_resampling(
+        rng::AbstractRNG,
+        weights::AbstractVector{<:Real},
+        N::Integer = length(weights)
+    )
+    return rand(rng, Categorical(weights), N)
+end
+
+# TODO: improve particle storage
+struct ParticleContainer{T, WT<:Real}
+    vals::Vector{T}
+    log_weights::Vector{WT}
+end
+
+Base.collect(pc::ParticleContainer) = pc.vals
+Base.length(pc::ParticleContainer) = length(pc.vals)
+Base.keys(pc::ParticleContainer) = LinearIndices(pc.vals)
+
+Base.@propagate_inbounds Base.getindex(pc::ParticleContainer, i::Int) = pc.vals[i]
+Base.@propagate_inbounds Base.getindex(pc::ParticleContainer, i::Vector{Int}) = pc.vals[i]
+Base.setindex!(pc::ParticleContainer{T}, p::T, i::Int) where T = Base.setindex!(pc.vals, p, i)
+
+## LINEAR GAUSSIAN STATE SPACE MODEL ##########################################
+
+struct LinearGaussianLatentDynamics{T<:Real} <: LatentDynamics
+    """
+        Latent dynamics for a linear Gaussian state space model.
+        The model is defined by the following equations:
+        x[t] = Ax[t-1] + ε[t],      ε[t] ∼ N(0, Q)
+    """
+    A::Matrix{T}
+    Q::PDMat{T, Matrix{T}}
+end
+
+# Convert covariance matrices to PDMats to avoid recomputing Cholesky factorizations
+function LinearGaussianLatentDynamics(A::Matrix, Q::Matrix)
+    return LinearGaussianLatentDynamics(A, PDMat(Q))
+end
+
+struct LinearGaussianObservationProcess{T<:Real} <: ObservationProcess
+    """
+        Observation process for a linear Gaussian state space model.
+        The model is defined by the following equation:
+        y[t] = Hx[t] + η[t],        η[t] ∼ N(0, R)
+    """
+    H::Matrix{T}
+    R::PDMat{T, Matrix{T}}
+end
+
+function LinearGaussianObservationProcess(B::Matrix, R::Matrix)
+    return LinearGaussianObservationProcess(B, PDMat(R))
+end
+
+function SSMProblems.distribution(
+        proc::LinearGaussianLatentDynamics{T},
+        extra
+    ) where {T<:Real}
+        dx = size(proc.A, 1)
+    return MvNormal(zeros(T, dx), diagm(ones(T, dx)))
+end
+
+function SSMProblems.distribution(
+        proc::LinearGaussianLatentDynamics{T},
+        step::Int,
+        state::AbstractVector{T},
+        extra
+    ) where {T<:Real}
+    return MvNormal(proc.A*state, proc.Q)
+end
+
+function SSMProblems.distribution(
+        proc::LinearGaussianObservationProcess{T},
+        step::Int,
+        state::AbstractVector{T},
+        extra
+    ) where {T<:Real}
+    return MvNormal(proc.H*state, proc.R)
+end
+
+const LinearGaussianModel{T<:Real} = StateSpaceModel{
+    LinearGaussianLatentDynamics{T},
+    LinearGaussianObservationProcess{T}
+}
+
+Base.eltype(::LinearGaussianModel{T}) where T = T
+Base.eltype(::StateSpaceModel) = error("model element type must be explicit")
+
+## FILTERING ##################################################################
+
+abstract type AbstractFilter <: AbstractSampler end
+
+"""
+    predict([rng,] states, model, alg, [step, extra])
+
+propagate the filtered states forward in time.
+"""
+function predict end
+
+"""
+    update(states, model, data, alg, [step, extra])
+
+update beliefs on the propagated states.
+"""
+function update end
+
+"""
+    prior([rng,] model, alg, [extra])
+
+propose an initial state distribution.
+"""
+function prior end
+
+function sample(
+        rng::AbstractRNG,
+        model::StateSpaceModel,
+        observations::AbstractVector,
+        filter::AbstractFilter
+    )
+    MT = eltype(model)
+    
+    filtered_states = prior(rng, model, filter, nothing)
+    log_evidence = zero(MT)
+
+    for t in eachindex(observations)
+        proposed_states = predict(
+            rng, filtered_states, model, filter, t, nothing
+        )
+
+        filtered_states, log_marginal = update(
+            proposed_states, model, observations[t], filter, t, nothing
+        )
+
+        log_evidence += log_marginal
+    end
+
+    return filtered_states, log_evidence
+end
+
+## KALMAN FILTER ##############################################################
+
+struct KalmanFilter <: AbstractFilter end
+
+KF() = KalmanFilter()
+
+function prior(
+        rng::AbstractRNG,
+        model::LinearGaussianModel,
+        filter::KalmanFilter,
+        extras
+    )
+    init_dist = SSMProblems.distribution(model.dyn, extras)
+    return Gaussian(init_dist.μ, Matrix(init_dist.Σ))
+end
+
+function predict(
+        rng::AbstractRNG,
+        particles::Gaussian,
+        model::LinearGaussianModel,
+        filter::KalmanFilter,
+        step::Integer,
+        extra
+    )
+    @unpack A, Q = model.dyn
+
+    predicted_particles = let μ = particles.μ, Σ = particles.Σ
+        Gaussian(A*μ, A*Σ*A' + Q)
+    end
+
+    return predicted_particles
+end
+
+function update(
+        proposed_particles::Gaussian,
+        model::LinearGaussianModel,
+        observation,
+        filter::KalmanFilter,
+        step::Integer,
+        extra
+    )
+    @unpack H, R = model.obs
+
+    particles, residual, S = GaussianDistributions.correct(
+        proposed_particles,
+        Gaussian(observation, R), H
+    )
+
+    log_marginal = logpdf(
+        Gaussian(zero(residual), Symmetric(S)),
+        residual
+    )
+
+    return particles, log_marginal
+end
+
+## BOOTSTRAP FILTER ###########################################################
+
+# TODO: adaptive resampling, set by default to always resmaple
+struct BootstrapFilter <: AbstractFilter
+    N::Int64
+    threshold::Float64
+end
+
+BF(N::Integer) = BootstrapFilter(N, 1.0)
+
+resample_threshold(filter::BootstrapFilter) = filter.threshold*filter.N
+
+function prior(
+        rng::AbstractRNG,
+        model::StateSpaceModel,
+        filter::BootstrapFilter,
+        extra
+    )
+    init_dist = SSMProblems.distribution(model.dyn, extra)
+    initial_states = map(
+        x -> rand(rng, init_dist),
+        1:filter.N
+    )
+
+    return ParticleContainer(initial_states, zeros(eltype(model), filter.N))
+end
+
+function predict(
+        rng::AbstractRNG,
+        particles::ParticleContainer,
+        model::StateSpaceModel,
+        filter::BootstrapFilter,
+        step::Integer,
+        extra
+    )
+    weights = softmax(particles.log_weights)
+    idx = multinomial_resampling(rng, weights)
+
+    proposed_states = map(
+        x -> SSMProblems.simulate(rng, model.dyn, step, x, extra),
+        particles[idx]
+    )
+
+    return ParticleContainer(proposed_states, particles.log_weights)
+end
+
+function update(
+        particles::ParticleContainer,
+        model::StateSpaceModel,
+        observation,
+        filter::BootstrapFilter,
+        step::Integer,
+        extra
+    )
+    log_marginals = map(
+        x -> SSMProblems.logdensity(model.obs, step, x, observation, extra),
+        collect(particles)
+    )
+
+    return (
+        ParticleContainer(particles.vals, log_marginals),
+        logsumexp(log_marginals) - convert(eltype(model), log(filter.N))
+    )
+end
